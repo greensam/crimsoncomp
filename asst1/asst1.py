e0bb5afc5d0ae642f51d3241715ef663d14486f2
Samuel Green <green.sam1192@gmail.com>
2017-03-12 22:45:24 -0400

    finish asst1

---
+++ asst1/asst1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import collections
+import csv
+
+def swapchars(s):
+
+    counts = collections.Counter(s.lower())
+
+    most = max(counts, key=counts.get)
+    least = min(counts, key=counts.get)
+
+    return s.replace(most, least)
+
+assert swapchars("samuel lewis green") == "samual lawis graan"
+print  swapchars('I\'m just a chi-town coder with a nice flow.') # == "U'm jist a chu-town coder wuth a nuce flow."
+
+
+def sortcat(*arg):
+    n = arg[0]
+    strings = arg[1:]
+    strings = sorted(strings, key=lambda l: len(l), reverse=True)
+
+    if n == -1:
+        return "".join(strings)
+    else:
+        return "".join(strings[:n])
+
+assert sortcat(1, 'abc', 'bc') == 'abc'
+assert sortcat(2, 'bc', 'c', 'abc') == 'abcbc'
+
+def readdic():
+    data = {}
+    with open('states.txt') as f:
+        reader = csv.reader(f)
+
+        for state, abv in reader:
+            data[abv] = state
+    return data
+
+DICT = readdic()
+
+def bluesclues(abbv):
+    
+    return DICT[abbv]
+
+def bluesbooze(state):
+    rev = {state:abbv for abbv, state in DICT.iteritems()}
+    return rev[state]
+
+assert bluesclues('AL') == 'Alabama'
+assert bluesclues('NY') == 'New York'
+assert bluesbooze('New York') == 'NY'
+assert bluesbooze('Connecticut') == 'CT'
+
